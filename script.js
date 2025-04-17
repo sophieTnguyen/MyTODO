eef90ff2b9d29b04603dfabac95b06fc9e7cdae7
Sophie Nguyen <sophie.thi.nguyen11@gmail.com>
2025-04-17 11:06:07 -0700

    Added checkbox functionality and changed createTask() to constructor Task()

--- script.js
+++ script.js
@@ -1,27 +1,50 @@
 window.addEventListener("DOMContentLoaded", function(){
     const taskList = document.getElementById('task-list');
-    const taskForm = document.querySelector('input');
+    const taskForm = document.getElementById('taskNameForm');
     const submitButton = document.querySelector('form');
     
     submitButton.addEventListener("submit", function(event) {
         event.preventDefault();
-        let task = createTask(taskForm.value);
+        console.log(taskForm.value, taskForm)
+        let task = new Task(taskForm.value);
         task.Load();
         taskForm.value = '';
     })
 
-    function createTask(text) 
+    function Task(text) 
     {
-        return {
-            text,
-            Load: function() {
-                const myTask = document.createElement('li');
-                taskList.appendChild(myTask);
-                myTask.className = "list-group-item";
-                myTask.innerText = text;
-            }
+        this.text = text;
+        this.Load =  function() {
+
+            const li = document.createElement('li');
+            li.className = 'list-group-item';
+
+            const div = document.createElement('div');
+            div.className = 'form-check';
+
+            const checkbox = document.createElement('input');
+            checkbox.className = 'form-check-input';
+            checkbox.type = 'checkbox';
+            // ID
+
+            const label = document.createElement('label');
+            label.className = 'form-check-label';
+            label.htmlFor = checkbox.id;
+            label.innerText = this.text;
+            console.log(this.text, this)
+
+            div.appendChild(checkbox);
+            div.appendChild(label);
+            li.appendChild(div);
+            taskList.appendChild(li);
+
+            checkbox.addEventListener("change", function() 
+            {
+                li.remove();
+            });
+        };
     
-        }
-    }   
-});
+    };
+    }
+)
 
